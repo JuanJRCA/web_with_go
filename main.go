6ae3a32225549403c2276131c40385a4aab2f502
juanrubio <juanrubiodatascience@gmail.com>
2020-05-27 00:05:14 +0200

    First web server

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func HandleFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Primera web </h1>")
+}
 
 func main() {
-	fmt.Println("Hello World !")
+	http.HandleFunc("/", HandleFunc)
+	http.ListenAndServe(":8080", nil)
 }
