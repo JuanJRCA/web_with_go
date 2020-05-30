d46ff2309072ef1516cd42d3410fa672f373d304
juanrubio <juanrubiodatascience@gmail.com>
2020-05-30 12:09:54 +0200

    Easy routing

--- main.go
+++ main.go
@@ -7,10 +7,16 @@ import (
 
 func HandleFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-type", "text/html")
-	fmt.Fprint(w, "<h1>Vamos a jugar al Mario </h1>")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Vamos a jugar al Mario </h1>")
+	}
+	if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "<h1>Página contacto</h1>")
+	}
 }
 
 func main() {
+
 	http.HandleFunc("/", HandleFunc)
 	http.ListenAndServe(":8080", nil)
 }
